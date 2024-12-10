1007e65d2e3466a723a2a7bd3e6ea5aa2b563a77
whykalashwhy <kalashbhaiya@gmail.com>
2024-12-09 22:09:04 -0500

    removed unused page

--- back.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-// Serve files from the 'public' directory
-app.use("/files", express.static("public/files"));
-
-// Start the server
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
